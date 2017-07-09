603ff72df33887e186125413d6eda39e7e15d4e6
Simon Zelazny <zelazny@mailbox.org>
2017-07-09 16:34:08 +0200

    foo

---
+++ _bin/fix_egg.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xinput --set-prop "HID 04d9:2082" "Device Accel Profile" 0
+xinput --set-prop "HID 04d9:2082" "Device Accel Velocity Scaling"  1
+xinput --set-prop "HID 04d9:2082" "Device Accel Constant Deceleration" 4
