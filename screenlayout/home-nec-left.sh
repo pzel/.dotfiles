7a6ce61fe2d6453431733829bad29c2d8a7366e5
Simon Zelazny <zelazny@mailbox.org>
2019-10-14 15:11:05 +0200

    Move to inotify for kbd and monitor switchers

---
+++ screenlayout/home-nec-left.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1600x900 --pos 1920x300 --rotate normal --output DP1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off --output VIRTUAL1 --off
---
+++ screenlayout/office-nec-right.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1368x768 --pos 0x200 --rotate normal --output DP1 --mode 1920x1200 --pos 1368x0 --rotate normal --output DP2 --off --output VIRTUAL1 --off
