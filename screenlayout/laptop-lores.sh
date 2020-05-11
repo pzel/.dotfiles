770ef8a981a7a35ce32109187d2eb8d158b0e904
Simon Zelazny <zelazny@mailbox.org>
2020-05-11 11:49:25 +0200

    Mon 11 May 2020 11:49:25 AM CEST

--- screenlayout/laptop-lores.sh
+++ screenlayout/laptop-lores.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1280x720 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output VIRTUAL1 --off
